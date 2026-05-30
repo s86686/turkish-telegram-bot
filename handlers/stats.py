03110bb34eb896bf0ba880f4e3f851a6a523662c
s86686 <s86686@yahoo.com>
2026-05-30 19:03:05 +0500

    Update stats.py

--- handlers/stats.py
+++ handlers/stats.py
@@ -1,1 +1,17 @@
+from aiogram import Router
 
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(
+    lambda m: m.text == "📈 Статистика"
+)
+async def stats(
+    message: Message
+):
+
+    await message.answer(
+        "📈 Статистика пока недоступна."
+    )
