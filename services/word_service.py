2febf51b2bcefa6f474f275a88e396b86b0c18e0
s86686 <s86686@yahoo.com>
2026-05-30 21:54:18 +0500

    Update word_service.py

--- services/word_service.py
+++ services/word_service.py
@@ -1,7 +1,13 @@
+# services/word_service.py
+
 import random
 
 from db.database import SessionLocal
 from db.models import Word
+
+from services.quiz_service import (
+    build_quiz
+)
 
 
 def get_random_word():
@@ -17,9 +23,39 @@ def get_random_word():
         if not words:
             return None
 
-        return random.choice(
+        word = random.choice(
             words
         )
+
+        result = {
+            "id": word.id,
+            "lemma": word.lemma,
+            "translation": word.translation,
+            "examples": [
+                {
+                    "tr": word.example_tr,
+                    "ru": word.example_ru
+                }
+            ]
+        }
+
+        all_words = []
+
+        for w in words:
+
+            all_words.append(
+                {
+                    "id": w.id,
+                    "translation": w.translation
+                }
+            )
+
+        result["quiz"] = build_quiz(
+            result,
+            all_words
+        )
+
+        return result
 
     finally:
 
