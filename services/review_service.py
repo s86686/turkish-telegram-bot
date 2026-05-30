9f5e5aeea4b93d29cf4f1c1f04cdf7a46ee39d00
s86686 <s86686@yahoo.com>
2026-05-30 20:49:36 +0500

    Create review_service.py

---
+++ services/review_service.py
@@ -0,0 +1,80 @@
+from datetime import (
+    datetime,
+    timedelta
+)
+
+from db.database import SessionLocal
+
+from db.models import (
+    User,
+    UserWord
+)
+
+
+def save_review(
+    telegram_id: int,
+    word_id: int,
+    quality: int
+):
+
+    db = SessionLocal()
+
+    try:
+
+        user = (
+            db.query(User)
+            .filter(
+                User.telegram_id == telegram_id
+            )
+            .first()
+        )
+
+        if not user:
+            return
+
+        user_word = (
+            db.query(UserWord)
+            .filter(
+                UserWord.user_id == user.id,
+                UserWord.word_id == word_id
+            )
+            .first()
+        )
+
+        if not user_word:
+
+            user_word = UserWord(
+                user_id=user.id,
+                word_id=word_id
+            )
+
+            db.add(user_word)
+
+        intervals = {
+            0: 1,
+            1: 2,
+            2: 5,
+            3: 10
+        }
+
+        interval = intervals.get(
+            quality,
+            1
+        )
+
+        user_word.interval_days = interval
+
+        user_word.next_review = (
+            datetime.utcnow()
+            + timedelta(days=interval)
+        )
+
+        if quality >= 2:
+            user_word.correct_count += 1
+        else:
+            user_word.wrong_count += 1
+
+        db.commit()
+
+    finally:
+        db.close()
