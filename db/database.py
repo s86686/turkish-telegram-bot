fa747249adda884ddda52214306af49d2d66c8e6
s86686 <s86686@yahoo.com>
2026-05-30 18:57:02 +0500

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import sessionmaker
 
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
