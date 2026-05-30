21c737f9b80be082b18adae294eb255dc0ee88f3
s86686 <s86686@yahoo.com>
2026-05-30 22:59:46 +0500

    Update review_service.py

--- services/review_service.py
+++ services/review_service.py
@@ -43,77 +43,78 @@ def save_review(
 
         if not user_word:
 
-    user_word = UserWord(
-        user_id=user.id,
-        word_id=word_id
-    )
+            user_word = UserWord(
+                user_id=user.id,
+                word_id=word_id
+            )
 
-    db.add(user_word)
+            db.add(user_word)
 
-if quality == 0:
+        if quality == 0:
 
-    user_word.repetitions = 0
-    user_word.interval_days = 1
+            user_word.repetitions = 0
+            user_word.interval_days = 1
 
-elif quality == 1:
+        elif quality == 1:
 
-    user_word.repetitions = max(
-        0,
-        (user_word.repetitions or 0) - 1
-    )
+            user_word.repetitions = max(
+                0,
+                (user_word.repetitions or 0) - 1
+            )
 
-    user_word.interval_days = 2
+            user_word.interval_days = 2
 
-elif quality == 2:
+        elif quality == 2:
 
-    user_word.repetitions = (
-        user_word.repetitions or 0
-    ) + 1
+            user_word.repetitions = (
+                user_word.repetitions or 0
+            ) + 1
 
-    if user_word.repetitions == 1:
-        user_word.interval_days = 3
-    elif user_word.repetitions == 2:
-        user_word.interval_days = 7
-    else:
-        user_word.interval_days = int(
-            user_word.interval_days * 1.8
+            if user_word.repetitions == 1:
+                user_word.interval_days = 3
+            elif user_word.repetitions == 2:
+                user_word.interval_days = 7
+            else:
+                user_word.interval_days = int(
+                    user_word.interval_days * 1.8
+                )
+
+        elif quality == 3:
+
+            user_word.repetitions = (
+                user_word.repetitions or 0
+            ) + 1
+
+            if user_word.repetitions == 1:
+                user_word.interval_days = 5
+            elif user_word.repetitions == 2:
+                user_word.interval_days = 14
+            else:
+                user_word.interval_days = int(
+                    user_word.interval_days * 2.5
+                )
+
+        user_word.next_review = (
+            datetime.utcnow()
+            + timedelta(
+                days=user_word.interval_days
+            )
         )
 
-elif quality == 3:
+        if quality >= 2:
 
-    user_word.repetitions = (
-        user_word.repetitions or 0
-    ) + 1
+            user_word.correct_count = (
+                user_word.correct_count or 0
+            ) + 1
 
-    if user_word.repetitions == 1:
-        user_word.interval_days = 5
-    elif user_word.repetitions == 2:
-        user_word.interval_days = 14
-    else:
-        user_word.interval_days = int(
-            user_word.interval_days * 2.5
-        )
+        else:
 
-user_word.next_review = (
-    datetime.utcnow()
-    + timedelta(
-        days=user_word.interval_days
-    )
-)
+            user_word.wrong_count = (
+                user_word.wrong_count or 0
+            ) + 1
 
-if quality >= 2:
-
-    user_word.correct_count = (
-        user_word.correct_count or 0
-    ) + 1
-
-else:
-
-    user_word.wrong_count = (
-        user_word.wrong_count or 0
-    ) + 1
-
-db.commit()
+        db.commit()
 
     finally:
+
         db.close()
