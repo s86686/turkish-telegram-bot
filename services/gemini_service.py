60587c8a6cab9609f1e7503f4c5fba7b64ea81ce
s86686 <s86686@yahoo.com>
2026-06-06 14:55:22 +0500

    Create gemini_service.py

---
+++ services/gemini_service.py
@@ -0,0 +1,43 @@
+from google import genai
+
+from config import GEMINI_API_KEY
+
+
+client = genai.Client(
+    api_key=GEMINI_API_KEY
+)
+
+
+def explain_phrase(
+    phrase: str
+) -> str:
+
+    prompt = f"""
+Ты преподаватель турецкого языка.
+
+Разбери турецкую фразу.
+
+Покажи:
+
+1. Перевод
+2. Разбор слов
+3. Краткое объяснение грамматики
+4. Один пример
+
+Фраза:
+
+{phrase}
+"""
+
+    try:
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
+
+        return response.text
+
+    except Exception as e:
+
+        return f"Ошибка Gemini: {e}"
