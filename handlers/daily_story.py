3052bdcb0158ee8ba66ffefdccc7be68f7748b96
s86686 <s86686@yahoo.com>
2026-06-13 15:39:59 +0500

    Update daily_story.py

--- handlers/daily_story.py
+++ handlers/daily_story.py
@@ -1,13 +1,15 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from services.daily_story_service import get_daily_story, get_user_words_for_story, create_daily_story
+from services.daily_story_service import get_daily_story, get_user_words_for_story, create_daily_story, filter_unknown_words
 from aiogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
     CallbackQuery
 )
 from aiogram.enums import ParseMode
-
+from services.gemini_service import (
+    extract_new_words
+)
 router = Router()
 
 daily_story_keyboard = InlineKeyboardMarkup(
@@ -60,19 +62,41 @@ async def show_daily_story(message: Message):
         words
     )
 
-    if not story or not story.story_text:
-
-        await message.answer(
-            "⚠️ Не удалось сгенерировать историю. Попробуйте позже."
-        )
-
-        return
+if not story or not story.story_text:
 
     await message.answer(
-        story.story_text,
-        reply_markup=daily_story_keyboard,
-        parse_mode=ParseMode.HTML
+        "⚠️ Не удалось сгенерировать историю. Попробуйте позже."
     )
+
+    return
+
+new_words = extract_new_words(
+    story.story_text
+)
+
+new_words = filter_unknown_words(
+    new_words
+)
+
+if new_words:
+
+    debug_text = "\n\n🆕 Найдено новых слов:\n\n"
+
+    for word in new_words:
+
+        debug_text += (
+            f"• {word['lemma']} — "
+            f"{word['translation']} "
+            f"({word['topic']})\n"
+        )
+
+    story.story_text += debug_text
+
+await message.answer(
+    story.story_text,
+    reply_markup=daily_story_keyboard,
+    parse_mode=ParseMode.HTML
+)
     
 @router.callback_query(lambda c: c.data == "close_story")
 async def close_story(callback: CallbackQuery):
